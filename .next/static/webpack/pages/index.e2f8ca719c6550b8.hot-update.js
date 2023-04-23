93b418d8b2a4b8e3490f282cda15e0f128afb6ad
Zeriab Chiah <zeriab@hotmail.com>
2023-04-23 21:12:44 +0300

    slider

---
+++ .next/static/webpack/pages/index.e2f8ca719c6550b8.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./src/data/Intro-with-horizontal.json":
+/*!*********************************************!*\
+  !*** ./src/data/Intro-with-horizontal.json ***!
+  \*********************************************/
+/***/ (function(module) {
+
+module.exports = JSON.parse('[{"id":1,"title":{"first":"Innovative Architecture"},"content":{"first":"Building beyond boundaries:","second":"Explore the power of innovative architecture with us."},"image":"/assets/images/slider/1921-1442.png"},{"id":2,"title":{"first":"Interior","second":"Classic and Modern"},"content":{"first":"Transforming spaces into dreams: ","second":"Experience the magic of our interior design"}},{"id":3,"title":{"first":"Elegant & Unique Design"},"content":{"first":"Crafting elegance, tailoring uniqueness","second":"Our designs are as exceptional as you are."},"image1":"/assets/images/slider/1920-1153.png"},{"id":4,"title":{"first":"Furniture design & manufacture"},"content":{"first":"Designing dreams, crafting passions: ","second":"Our furniture is the heart and soul of your space."},"image":"/assets/images/slider/1919-1066.png"}]');
+
+/***/ })
+
+});
--- .next/static/webpack/webpack.6d8752ef2ec40f63.hot-update.js
+++ .next/static/webpack/webpack.6d8752ef2ec40f63.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "2d9b9c8565592825"; }
+/******/ 	__webpack_require__.h = function() { return "ecfc373275031a94"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
